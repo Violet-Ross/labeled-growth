e972387ae9106c8f065a811dca33e3caf09cf00a
Cataldo <fcataldo@ada.middlebury.edu>
2026-04-28 10:42:54 -0400

    highschool processed

---
+++ scripts/data_cleaning/highschool_csv_to_xgi.py
@@ -0,0 +1,75 @@
+import xgi
+import pandas as pd
+import numpy as np
+import networkx as nx
+
+# read in csv files
+contacts = np.array(pd.read_csv('data/highschool/HighSchool2013_proximity_net.csv', header=None))
+
+hyperedges = []
+class_labels = {}
+g = nx.Graph()
+
+prev_timestep = None
+for contact in contacts:
+    # get each line of csv, split into attributes
+    timestep, index0, index1, class0, class1 = contact[0].split(" ")
+
+    # add class labels to nodes
+    class_labels[(index0)] = class0
+    class_labels[(index1)] = class1
+
+    if prev_timestep == None:
+        prev_timestep = timestep
+
+    # want interaction hyperedge where all people interacting in one timestep are in a hyperedge... 
+    # can be multiple, so can't add all to hyperedge
+
+    if prev_timestep != timestep:
+        # print("timestep: " + str(prev_timestep))
+        prev_timestep = timestep
+        for hyperedge in nx.connected_components(g):
+            hyperedges.append(hyperedge)
+            # print("hyperedge: " + str(hyperedge))
+        
+        # clear graph
+        g = nx.Graph()
+    g.add_edge(index0, index1)
+
+
+# create hypergraph from the edgelist
+H = xgi.Hypergraph(hyperedges)
+
+
+# make 9 classes into binary labeled
+for node, class_label in class_labels.items():
+    if class_label == "2BIO1" or class_label == "2BIO2" or class_label == "2BIO3":
+        class_labels[node] = 1
+    else:
+        class_labels[node] = 0
+
+# set the node labels
+H.set_node_attributes(class_labels, name = "label")
+
+# want to index the nodes from 0 to n, the number of nodes
+
+# store old id
+old_labels = {n: n for n in H.nodes}
+H.set_node_attributes(old_labels, name="dataset_id")
+
+# make new hypergraph with correct indexing
+H2 = xgi.Hypergraph()
+mapping = {old: i for i, old in enumerate(H.nodes)}
+
+# Add nodes with attributes
+for old_node in H.nodes:
+    new_node = mapping[old_node]
+    H2.add_node(new_node, **H.nodes[old_node])
+
+# add edges
+for edge in H.edges:
+    new_edge = [mapping[n] for n in H.edges.members(edge)]
+    H2.add_edge(new_edge, **H.edges[edge])
+
+# save the hypergraph locally
+xgi.write_json(H2, "throughput/highschool.json")
