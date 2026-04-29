c012de4f93802000524870c92025f015ee49eac4
Violet Ross <violet.ross@colorado.edu>
2026-04-29 08:14:50 -0600

    scripts for SEM on high school class data

---
+++ scripts/senate_plots.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# slurm template for serial jobs
+# Set SLURM options
+#SBATCH --job-name=serial_test # Job name
+#SBATCH --output=serial_test-%j.out
+# Standard output and error log
+#SBATCH --mail-user=username@middlebury.edu
+# Where to send mail
+#SBATCH --mail-type=NONE
+# Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mem=2G
+# Job memory request 
+#SBATCH --partition=standard
+# Partition (queue) 
+#SBATCH --time=48:00:00
+# Time limit hrs:min:sec 
+
+# Run python script
+cd /home/vross/labeled-growth
+export PYTHONPATH=/home/vross/labeled-growth:$PYTHONPATH
+python3 scripts/senate_plots.py
