a2999da2f72f7341fd2bfb0078297ef8021701e1
Phil Chodrow <pchodrow@middlebury.edu>
2026-05-18 14:55:22 -0400

    new figs for asymptotics;

---
+++ scripts/degree-distributions.py
@@ -0,0 +1,107 @@
+import sys 
+sys.path.append("src")
+sys.path.append("scripts")
+import poisson_hypergraph as ph 
+from linear_map import matrix_of_linear_map
+
+import xgi 
+from collections import Counter
+import numpy as np
+from matplotlib import pyplot as plt
+plt.style.use('seaborn-v0_8-whitegrid')
+
+theta = [0.7, 0.3, 0.5, 0.3, 0.2, 0.1]
+
+H = xgi.Hypergraph([[0, 1]])
+H.set_node_attributes({0 : 0, 1 : 1}, name = "label")
+g = ph.GH(H, [0, 1], theta[0], theta[1])
+
+# compute the slope of the degree distribution from the matrix of the linear map
+
+k_max = 12
+M = matrix_of_linear_map(k_max, theta)
+vals, E = np.linalg.eig(M)
+idx = vals.argsort()[::-1]
+v = E[:, idx[0]].real
+v = v / v.sum()
+P = v.reshape((k_max + 1, k_max + 1))
+
+# compute some moments of the degree distribution
+
+K_0 = np.arange(k_max + 1)[:, None] 
+K_1 = np.arange(k_max + 1)[None, :]
+K = K_0 + K_1
+
+rho_0  = np.nansum((K_0**2/K)*P)
+rho_1  = np.nansum((K_1**2/K)*P)
+rho_01 = np.nansum((K_0*K_1/K)*P)
+
+mean_d = np.nansum(K*P) / (theta[4] + theta[5])
+
+a = (1 + theta[0]*(rho_0 + rho_1 - 1) + 2*theta[1]*rho_01)/mean_d
+
+print(f"Mean nodes copied per edge: {a}")
+
+# need to review math and re-check this, probably not right as is
+zeta = 1 + (theta[4] + theta[5]) / a 
+
+def degree_sequence(g):
+    edge_list = g.H.edges.members()
+    deg_seq = Counter()
+    for edge in edge_list:
+        for node in edge:
+            deg_seq[node] += 1
+    return np.array(list(deg_seq.values()))
+
+def log_binned_histogram(degree_sequence, interval = 5, num_bins = 20):
+    hist, bins = np.histogram(degree_sequence, bins = min(int(len(degree_sequence)/interval), num_bins))
+    bins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
+    hist, bins = np.histogram(degree_sequence, bins = bins)
+    binwidths = bins[1:] - bins[:-1]
+    hist = hist / binwidths
+    p = hist/hist.sum()
+
+    return bins[:-1], p
+
+for i in range(10000): 
+    g.add_hyperedge(2, theta[2], theta[3], theta[4], theta[5])
+    
+d = degree_sequence(g)
+
+fig, ax = plt.subplots(figsize = (5,4))
+
+centers, heights = log_binned_histogram(d, num_bins = 30)
+
+dmax = d.max()
+dmin = d.min()
+
+upper = dmax 
+lower = dmax / 10
+
+offset = (0.2, 0.002)
+
+x = np.linspace(max(100, lower), upper, 10)*offset[0]
+
+y = (x ** (-zeta)) 
+
+y = offset[1]*y / y.sum()
+
+ax.plot(x, y, linestyle = "--", color = "black", zorder = -10, linewidth=1)
+ax.annotate(fr"$\zeta=${zeta:.2f}", xy = (x[0]*2, y[0]/2))
+
+ax.scatter(centers, heights, facecolors='none', edgecolors =  '#055775', linewidth = 2)
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlabel("Degree")
+ax.set_ylabel("Density")
+
+# title = fr"""
+# Degree distribution
+# $\eta_+ = {theta[0]}, \lambda_+ = {theta[2]}, \gamma_+ = {theta[4]}$
+# $\eta_- = {theta[1]}, \lambda_- = {theta[3]}, \gamma_- = {theta[5]}$
+# """
+
+# ax.set_title(title)
+
+plt.tight_layout()
+plt.savefig("fig/degree_distribution.png")
--- scripts/stationarity.py
+++ scripts/stationarity.py
@@ -31,6 +31,8 @@ if __name__ == "__main__":
     # spectral gap analysis
     
     k_max = 12
+    # k_max = 8
+    
     grid_resolution = 9
     gaps = np.zeros((grid_resolution, grid_resolution))
     
@@ -39,6 +41,7 @@ if __name__ == "__main__":
     
     for i, eta_plus in enumerate(ETA_PLUS):
         for j, eta_minus in enumerate(ETA_MINUS):
+            print(f"Computing spectral gap for eta_plus = {eta_plus:.1f}, eta_minus = {eta_minus:.1f}")
             theta = [eta_plus, eta_minus, 0.5, 0.2, 0.4, 0.2]
             M = matrix_of_linear_map(k_max, theta)
             vals, E = np.linalg.eig(M)
@@ -66,7 +69,8 @@ if __name__ == "__main__":
     
     # eta_plus_index = 8
     # eta_minus_index = 1
-    eta_plus_index = 1
+    # eta_plus_index = 8
+    eta_plus_index = 7
     eta_minus_index = 0
     eta_plus = ETA_PLUS[eta_plus_index]
     eta_minus = ETA_MINUS[eta_minus_index]
@@ -115,9 +119,9 @@ if __name__ == "__main__":
     
     # SECOND: simulation with the same parameters
     
-    n_steps = int(1e4)
-    project_every = 100
-    window = 100
+    n_steps = int(1e7)
+    project_every = 1000
+    window = 1000
     
     TS = ToySimulator(edge_list = [[5,5]], theta = theta, force_label = None)
 
@@ -147,21 +151,18 @@ if __name__ == "__main__":
             
     
     # fig = plt.figure(figsize = (11, 7))    
-    fig = plt.figure(figsize = (12, 8))
-    
-    cbar_ax_width = 0.1
-    
-    widths = [1, cbar_ax_width,  1, 1, cbar_ax_width]
-    gs = GridSpec(2, 5, figure=fig, width_ratios=widths, hspace = 0.3, wspace = 0.0)
+    fig = plt.figure(figsize = (13, 7))
+    widths = [1,  1, 1]
+    gs = GridSpec(2, 3, figure=fig, width_ratios=widths, hspace = 0.35, wspace = 0.2)
     
     axes = []
     
-    for i in range(5):
+    for i in range(3):
         axes.append(fig.add_subplot(gs[0, i]))
     
-    axes.append(fig.add_subplot(gs[1, 0:3]))
-    axes.append(fig.add_subplot(gs[1, 3]))
-    axes.append(fig.add_subplot(gs[1, 4]))
+    axes.append(fig.add_subplot(gs[1, 0:2]))
+    axes.append(fig.add_subplot(gs[1, 2]))
+    
     
     
     # num_eigvecs = 2
@@ -172,17 +173,18 @@ if __name__ == "__main__":
     # fig, ax = plt.subplots(1, 4, figsize = (18, 6))
     
     colors = ["steelblue", "darkgrey", "lightgrey"]
+    letter_labels = ["(b)", "(c)"]
     
     for i in range(2):
         vi = np.real(E[:, i])
-        val = np.real(vals[i])
+        val = np.real(vals[i]) if i > 0 else 1.0
         vi = vi / vi.sum()
         vi = vi / np.abs(vi).sum()
         P = vi.reshape((k_max + 1, k_max + 1))
         
-        ax = axes[i+2]
+        ax = axes[i+1]
         im = ax.imshow(P, cmap = "BrBG", interpolation = "none", vmin = v_min, vmax = vmax, origin = "lower")
-        ax.set_title(rf"$v_{i+1}$ ($\lambda_{i+1} = {val:.2f}$)")
+        ax.set_title(rf"{letter_labels[i]}: $v_{i+1}$ ($\lambda_{i+1} = {val:.2f}$)")
         ax.set_xlabel(r"$k_0$")
         if i == 0: 
             ax.set_ylabel(r"$k_1$")
@@ -190,15 +192,16 @@ if __name__ == "__main__":
         ax.set_xticks(range(0, k_max + 1, 3))
         ax.set_yticks(range(0, k_max + 1, 3))
         
-        ax = axes[5]
+        ax = axes[3]
         ax.plot(projection_timesteps, projections[i], label = fr"$v_{i+1}$", color = colors[i])
         ax.legend()
         ax.semilogx()
-        ax.set_title("Projection of simulation state onto eigenvectors")
+        ax.set_title("(d): Projection of simulation state onto eigenvectors")
+    
+
     
     
-    
-    ax = axes[5]
+    ax = axes[3]
     ax.plot([min(projection_timesteps), n_steps], [0, 0], "k--", alpha = 0.5)
     ax.set_xlabel("Timestep")
     ax.set_ylabel("Cosine similarity")
@@ -207,11 +210,11 @@ if __name__ == "__main__":
     
     #     fig.colorbar(im, ax = ax[i])
     
-    ax = axes[6]
+    ax = axes[4]
     final_window = n_steps
     C = get_dist(TS, final_window, k_max)
     final = ax.imshow(C / C.sum(), cmap = "Greys", interpolation = "none", vmin = 0, origin = "lower")
-    ax.set_title(f"Final simulation state")
+    ax.set_title(f"(e): Final simulation state")
     
     ax.set_xlabel(r"$k_0$")
     ax.set_ylabel(r"$k_1$")
@@ -219,13 +222,13 @@ if __name__ == "__main__":
     ax.set_xticks(range(0, k_max + 1, 3))
     ax.set_yticks(range(0, k_max + 1, 3))
     
-    fig.colorbar(im, cax = axes[4])
-    fig.colorbar(final, cax=axes[7])
+    fig.colorbar(im)
+    fig.colorbar(final)
     
     
     ax = axes[0]
     im = ax.imshow(gaps, cmap = "inferno", interpolation = "none", origin = "lower")
-    ax.set_title("Spectral gap")
+    ax.set_title("(a): Spectral gap")
     ax.set_xlabel(r"$\eta_+$")
     ax.set_ylabel(r"$\eta_-$")
     ax.set_xticks(range(grid_resolution))
@@ -233,12 +236,14 @@ if __name__ == "__main__":
     ax.set_yticks(range(grid_resolution))
     ax.set_yticklabels([f"{x:.1f}" for x in np.linspace(0.1, 0.9, grid_resolution)])
     
-    fig.colorbar(im, cax=axes[1])
+    fig.colorbar(im, ax = ax, format = "%.2f")
     
     
 
     # highlight the eta_plus and eta_minus values used in the simulation
     rect = patches.Rectangle((eta_plus_index - 0.5, eta_minus_index - 0.5), 1, 1, linewidth=2, edgecolor='white', facecolor='none')
     axes[0].add_patch(rect)
+    
 
     plt.savefig("fig/linear_map_eigenvectors.png", dpi = 300, bbox_inches = "tight")
+
