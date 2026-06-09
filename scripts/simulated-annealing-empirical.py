77bac61e2a89c7d7b058741ebe6835fb98d605ae
Cataldo <fcataldo@ada.middlebury.edu>
2026-06-08 20:10:11 -0400

    empirical senate bills tooling

--- scripts/simulated-annealing-empirical.py
+++ scripts/simulated-annealing-empirical.py
@@ -8,7 +8,6 @@ import argparse
 import itertools
 import pandas as pd
 # in a .py file, just this import works
-import argparse
 import xgi
 from src.poisson_hypergraph import GH
 from src.algorithms.simulated_annealing import SimulatedAnnealingApprox
@@ -22,7 +21,11 @@ from tqdm import tqdm
 
 
 data_sets = {
-    "senate_bills": "senate_bills"
+    "senate_bills": "senate_bills",
+    "highschool_class": "highschool",
+    "highschool_gender": "highschool_gender",
+    "primaryschool_gender": "primaryschool_gender",
+    "primaryschool_class": "primaryschool"
 }
 
 if __name__ == "__main__":
@@ -31,11 +34,23 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser(description='Run simulated annealing on a user-specified prepared data set.')
     args.add_argument('--data_set', type=str, choices=data_sets.keys(), help='The data set to run simulated annealing on. Must be one of: ' + ', '.join(data_sets.keys()))
     args.add_argument("--job_id", type=int, help="The job ID for this run, used to differentiate output files when running multiple jobs.")
-    
+    args.add_argument("--results_folder", type=str, help="Folder to store results (defaults to data set name)", default=None)
+    args.add_argument("--params", type=float, nargs=6, help="Specified simulated annealingparameters to override default.  6-tuple of floats in the form (p+, p-, a+, a-, r+, r-)", default=None)
+
     args = args.parse_args()
     
     data_set = args.data_set
     job_id = args.job_id
+    params = args.params
+
+    results_folder = data_sets[data_set] # set default name of results folder to the title of dataset
+    if args.results_folder != None:
+        results_folder = args.results_folder # override if provided
+
+    # order is copy, novel, extant
+    true_theta = [.95, .05, .001, .001, 1, .01]
+    if params != None: # override default params if user-specified
+        true_theta = params
     
     path = f"throughput/simulated_annealing/{data_sets[data_set]}"
     os.makedirs(path, exist_ok=True)
@@ -43,11 +58,11 @@ if __name__ == "__main__":
     os.makedirs(path + "/metrics", exist_ok=True)
     
     # initialize 
-    H = pkl.load(open(f"throughput/{data_sets[data_set]}.pkl", "rb"))
+    # H = pkl.load(open(f"throughput/{data_sets[data_set]}.pkl", "rb")) unsure of why this is used
+    H = xgi.read_json(f"throughput/{data_sets[data_set]}.json", nodetype=int)
     g = GH(H, [0, 1], 0, 0)
     
-    # order is copy, novel, extant
-    true_theta = [.95, .05, .001, .001, 1, .01]
+    
     
     print("initializing simulated annealing...")
     sa = SimulatedAnnealingApprox(g, true_theta)
---
+++ slurm/simulated-annealing-empirical-primaryschool-class.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=simulated-annealing-primaryschool-class
+#SBATCH --array=0-9
+#SBATCH --mail-user=fcataldo@middlebury.edu    
+#SBATCH --mail-type=ALL                        
+#SBATCH --mem-per-cpu=50gb
+#SBATCH --partition=himem-long                   
+#SBATCH --time=120:00:00        
+#SBATCH --output=throughput/logs/simulated-annealing-empirical/primaryschool-class_%a.out            
+
+.venv/bin/python -u -m scripts.simulated-annealing-empirical --data_set primaryschool_class --job_id $SLURM_ARRAY_TASK_ID --results_folder testing_folder --params .9 .1 .001 .001 1 .1
