de12b78eda35ee9de89b55173516cc7f4c566a74
Cataldo <fcataldo@ada.middlebury.edu>
2026-06-10 19:10:12 -0400

    added simulated annealing algorithm to appendix

--- slurm/simulated-annealing-empirical.sh
+++ slurm/simulated-annealing-empirical.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=simulated-annealing-senate
 #SBATCH --array=0-9
-#SBATCH --mail-user=pchodrow@middlebury.edu    
+#SBATCH --mail-user=fcataldo@middlebury.edu    
 #SBATCH --mail-type=ALL                        
 #SBATCH --mem-per-cpu=50gb
 #SBATCH --partition=himem-long                   
 #SBATCH --time=120:00:00        
 #SBATCH --output=throughput/logs/simulated-annealing-empirical/senate_bills_%a.out            
 
-env/bin/python -u -m scripts.simulated-annealing-empirical --data_set senate_bills --job_id $SLURM_ARRAY_TASK_ID
+.venv/bin/python -u -m scripts.simulated-annealing-empirical --data_set senate_bills --job_id $SLURM_ARRAY_TASK_ID
