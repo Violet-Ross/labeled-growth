3c4a2cd19c5a51bf80d13fa782a8c2c4ada74870
Phil Chodrow <pchodrow@middlebury.edu>
2026-06-16 16:42:38 -0400

    update community synthetic data runs with new initial condition

--- slurm/community-synthetic.sh
+++ slurm/community-synthetic.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=community-synthetic-sims
-#SBATCH --array=0-9
+#SBATCH --array=0-4
 #SBATCH --mail-user=pchodrow@middlebury.edu    
 #SBATCH --mail-type=ALL                        
 #SBATCH --mem-per-cpu=30gb
-#SBATCH --partition=himem-standard                   
+#SBATCH --partition=standard                   
 #SBATCH --time=24:00:00        
 #SBATCH --output=throughput/logs/community-synthetic/community_synthetic_sims_%a.out            
 
-env/bin/python -u -m scripts.community-synthetic-data --steps 100 --resolution 10 --runs 10 --job $SLURM_ARRAY_TASK_ID
+env/bin/python -u -m scripts.community-synthetic-data --steps 100 --resolution 10 --runs 20 --job $SLURM_ARRAY_TASK_ID
