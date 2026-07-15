15e81f94f64952c1c4dff78fc6625d7216fb34b3
violet <viro3403@colorado.edu>
2026-07-15 16:38:26 -0600

    fig 1

--- scripts/community-detection-learning-progress-example.py
+++ scripts/community-detection-learning-progress-example.py
@@ -1,193 +1,185 @@
-# if using a .ipynb in a folder, this heading is needed before importing
-import sys
-import os
+from src.poisson_hypergraph import GH
+from matplotlib import pyplot as plt
+import pandas as pd
+import os 
+import seaborn as sns
+
+import xgi
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+from src.algorithms.sem import sem_functions
+import pandas as pd
+sem = sem_functions()
 import scripts.figure_settings as fs
 
-project_root = os.path.abspath(os.path.join(os.getcwd(), '..'))
-sys.path.insert(0, project_root)
 
 
-# in a .py file, just this import works
-from src.poisson_hypergraph import GH # custom hypergraph class
-from src.algorithms.simulated_annealing import SimulatedAnnealingApprox
-from src.algorithms.gradient_descent import GradientDescent
-
-# generally used packages
-import xgi
-import csv
-
-from sklearn.metrics import adjusted_rand_score 
-import networkx as nx
-import numpy as np
-from itertools import combinations
-
-from matplotlib import pyplot as plt
-import seaborn as sns
 sns.set_style("whitegrid")
-
 fs.set_fonts()
 
 
-def generate_graph_26_starting_nodes(true_theta, timesteps):
-    true_p, true_q, gamma_nu, gamma_nr, gamma_eu, gamma_er = true_theta
-    H = xgi.Hypergraph([[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]])
-    H.set_node_attributes({0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1}, name="label")
-    g = GH(H, [0,1], true_p, true_q)
-    g.add_hyperedge(timesteps, gamma_nu, gamma_nr, gamma_eu, gamma_er)
+df = pd.DataFrame()
 
-    return g
-
-def initial_condition_GH(theta, timesteps):
-    eta_plus, eta_minus, gamma_nu, gamma_nr, gamma_eu, gamma_er = theta
-    H = xgi.Hypergraph([[0,1,2], [3, 4, 5]])
-    H.set_node_attributes({0:0,1:0,2:0,3:0,4:0,5:1}, name="label")
-    g = GH(H,[0, 1], eta_plus , eta_minus)
-    g.add_hyperedge(timesteps, gamma_nu , gamma_nr , gamma_eu , gamma_er)
-    return g
-
-
-
-# Modularity Maximization Implementation
-
-def clique_projection_modularity_maximization_algo(g):
-    H = g.H
-    G = nx.Graph()
-    G.add_nodes_from(H.nodes)
-
-    # Clique projection
-    for edge in H.edges.members():
-        for u, v in combinations(edge, 2):
-            if G.has_edge(u, v):
-                G[u][v]["weight"] += 1
-            else:
-                G.add_edge(u, v, weight=1)
-
-    partition = nx.community.greedy_modularity_communities(G, best_n=2, weight="weight")
-    z = np.array([0 if node in partition[0] else 1 for node in G.nodes()])
-
-    print(len(z))
-
-    return z
-
-def spectral_clustering_comparison(g): 
-    H = g.H
-    G = nx.Graph()
-    G.add_nodes_from(H.nodes)
-
-    # Clique projection
-    for edge in H.edges.members():
-        for u, v in combinations(edge, 2):
-            if G.has_edge(u, v):
-                G[u][v]["weight"] += 1
-            else:
-                G.add_edge(u, v, weight=1)
-
-    from sklearn.cluster import SpectralClustering
-
-    adjacency_matrix = nx.to_numpy_array(G, weight="weight")
-    spectral = SpectralClustering(n_clusters=2, affinity='precomputed', random_state=0)
-    labels = spectral.fit_predict(adjacency_matrix)
-
-    return labels
-
-if __name__ == "__main__":
-
-    # np.random.seed(12345)
-    # define params for hypergraph generation:
-    eta_plus = .9 # like-labeled copy param
-    eta_minus = .1 # unlike-labeled copy param
-    lambda_plus = 1 # like-labeled external param
-    lambda_minus = .25 # unlike-labeled external param
-    mu_plus = .2 # like-labeled novel param
-    mu_minus = .1 # unlike-labeled novel param
-
-    true_theta = [eta_plus, eta_minus, mu_plus, mu_minus, lambda_plus, lambda_minus]
-
-    # number of edges added to synthetic hypergraph
-    timesteps = 198
-
-    g = initial_condition_GH(true_theta, timesteps)
-        
-    
-    # in my code, frequently titled "true_theta"
-    _eta_plus = .9 # like-labeled copy param
-    _eta_minus = .1 # unlike-labeled copy param
-    _lambda_plus = 1 # like-labeled external param
-    _lambda_minus = .25 # unlike-labeled external param
-    _mu_plus = .2 # like-labeled novel param
-    _mu_minus = .1 # unlike-labeled novel param
-
-    # combine into format for algorithm
-    guessed_theta = [_eta_plus, _eta_minus, _mu_plus, _mu_minus, _lambda_plus, _lambda_minus]
-
-    sa = SimulatedAnnealingApprox(g, guessed_theta, approx=100, novel=True)
-
-    algo_timesteps = len(g.nodes)*20
-
-    for step_num in range(algo_timesteps):
-        sa.step()
-        if (step_num+1) % (algo_timesteps//10) == 0:
-            print(f"Completed {(step_num+1)/algo_timesteps:.0%} steps, current ll/edge: {sa.likelihoods_per_step[-1]/len(g.H.edges):.4f}")
-            
+for filename in os.listdir("throughput/community-structure-sims"):
+    if filename.endswith(".csv"):
+        df = pd.concat([df, pd.read_csv(f"throughput/community-structure-sims/{filename}")], ignore_index=True)
         
 
-    m = len(g.H.edges)
 
-    max_ll_index = np.argmax(sa.likelihoods_per_step)
+params = ["theta_1", "theta_2", "theta_3", "theta_4", "theta_5", "theta_6"]
 
-    fig, axarr = plt.subplots(1, 2, figsize=(10, 4))
-    axarr[0].plot(np.array(sa.likelihoods_per_step)/m, color = "#1B998B")
-    axarr[0].set_ylabel("Likelihood per edge")
-    axarr[0].set_xlabel("Step")
+agged = df.groupby(params).agg({"modularity" : "mean", "proportion_pure_edges" : "mean", "mean_majority_proportion" : "mean"}).reset_index()
 
-    axarr[0].scatter(max_ll_index, sa.likelihoods_per_step[max_ll_index]/m, color='black', label='Max LL', zorder = 10)
-    axarr[0].legend()
 
-    axarr[1].plot(sa.aris_per_step, color = "#1B998B")
-    axarr[1].set_ylabel("Adjusted Rand Index (ARI)")
-    axarr[1].set_xlabel("Step")
-    axarr[1].scatter(max_ll_index, sa.aris_per_step[max_ll_index], color='black', label='Max LL', zorder = 10)
-    axarr[1].legend()
+col_to_plot = "modularity"
 
-    modularity_labels = clique_projection_modularity_maximization_algo(g)
-    mod_ari = adjusted_rand_score(g.get_labels(), modularity_labels)
-    
-    spectral_labels = spectral_clustering_comparison(g)
-    spectral_ari = adjusted_rand_score(g.get_labels(), spectral_labels)
+agged["theta_2"].unique()
 
-    # axarr[1].plot([0, algo_timesteps], [mod_ari, mod_ari], color='black', label='Modularity ARI', linestyle='--', zorder = -10)
-    # axarr[1].legend()
-    axarr[1].plot([0, algo_timesteps], [spectral_ari, spectral_ari], color='black', label='Spectral Clustering', linestyle='--', zorder = -10)
-    axarr[1].legend()
+fig, axarr = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
+
+for i in range(2): 
+    theta_4 = agged["theta_4"].unique()[i]
+    sub = agged[agged["theta_4"] == theta_4]
+    sub["diff"] = sub["theta_1"] - sub["theta_2"]
     
     
-    plt.tight_layout()
+    sns.lineplot(data=sub, x="diff", y=col_to_plot, hue="theta_3", palette="inferno", ax=axarr[i], alpha = 0.5, legend = i == 0, linewidth = 2.5)
+    axarr[i].set_title(f"theta_4 = {theta_4}")
+    axarr[i].set_ylim(0, None)
 
-    plt.savefig("fig/community-detection-learning-progress.png", dpi=300, bbox_inches="tight")
+plt.savefig("fig/community-structure-sims-line.png", dpi=300, bbox_inches="tight")
+
+
+
+
+# COMBINED FIG 
+
+
+def clique_projection_modularity(GH): 
+    """
+    unweighted so possibly not that reliable, would want to use weighted graph instead but not implemented in XGI. Not a hard implementation but likely slow. 
+    """
     
-    tex_dict = {
-        "\simannviznumsteps": algo_timesteps,
-        "\simannviznumnodes": len(g.nodes),
-        "\simannviznumedges": len(g.H.edges),
-        "\simannvizmaxll": f"{sa.likelihoods_per_step[max_ll_index]/m:.2f}",
-        "\simannvizmaxllari": f"{sa.aris_per_step[max_ll_index]:.2f}",
-        "\simannvizmodularityari": f"{mod_ari:.2f}",
-        "\simannvizspectralari": f"{spectral_ari:.2f}",
-        "\simannvizsamecopyrate": f"{eta_plus:.2f}",
-        "\simannvizoppcopyrate": f"{eta_minus:.2f}",
-        "\simannvizsameextantrate": f"{lambda_plus:.2f}",
-        "\simannvizoppextantrate": f"{lambda_minus:.2f}",
-        "\simannvizsamenovelrate": f"{mu_plus:.2f}",
-        "\simannvizoppnovelrate": f"{mu_minus:.2f}",
-        "\simannvizsamecopyrateguess": f"{_eta_plus:.2f}",
-        "\simannvizoppcopyrateguess": f"{_eta_minus:.2f}",
-        "\simannvizsameextantrateguess": f"{_lambda_plus:.2f}",
-        "\simannvizoppextantrateguess": f"{_lambda_minus:.2f}",
-        "\simannvizsamenovelrateguess": f"{_mu_plus:.2f}",
-        "\simannvizoppnovelrateguess": f"{_mu_minus:.2f}"
-    }
+    A = xgi.linalg.hypergraph_matrix.adjacency_matrix(GH.H, weighted = True)
+    z = np.array(GH.get_labels())
+    k = A.sum(axis=1)
+    m = A.sum() / 2
+    Delta = z[:, None] == z[None, :] 
+    return 1/(2*m)*((A - np.outer(k, k) / (2 * m))*Delta).sum()
+
+
+fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+theta_4 = 0.5
+sub = agged[agged["theta_4"] == theta_4]
+
+palette = sns.color_palette("inferno_r", n_colors=sub["theta_3"].nunique())
+
+
+sns.lineplot(data=sub, x="theta_1", y=col_to_plot, hue="theta_3", palette=palette, ax=ax, alpha = 0.8, legend = True, linewidth = 2.5, zorder = 100)
+ax.set_ylim(0, None)
+ax.set_xlabel(rf"$\{fs.theta['copy']}_+ = 1 - \{fs.theta['copy']}_-$")
+ax.set_ylabel(r"$Q$")
+# change the legend title 
+# reverse the order of the legend items 
+
+handles, labels = ax.get_legend_handles_labels()
+handles, labels = reversed(handles), reversed(labels)
+
+new_labels = [f"$\{fs.theta['extant']}_+$ = {label}" for label in labels]
+legend = ax.legend(handles, 
+          new_labels, 
+          title = None)
+legend.set_zorder(1001)
+
+
+
+inset_1 = ax.inset_axes(
+   [1.0, 0.5, 0.5, 0.5]
+)
+
+inset_2 = ax.inset_axes(
+   [1.0, 0.0, 0.5, 0.5]
+)
+
+
+inset_3 = ax.inset_axes(
+   [-0.6, 0.5, 0.5, 0.5]
+)
+
+inset_4 = ax.inset_axes(
+   [-0.6, 0.0, 0.5, 0.5]
+)
+# we want to visualize a small hypergraph with two parameter sets corresponding to parts of the plot here. 
+
+n_steps = int(30)
+
+choices_to_highlight = [
+    {"theta_1" : 0.975, "theta_3" : 2.0},
+    {"theta_1" : 0.975, "theta_3" : 0.5},
+    {"theta_1" : 0.525, "theta_3" : 2.0},
+    {"theta_1" : 0.525, "theta_3" : 0.5},
+]
+
+
+for i, choice in enumerate(choices_to_highlight):
     
-    with open("paper/community-detection-learning-progress.tex", "w") as f:
-        for key, value in tex_dict.items():
-            f.write(f"\\newcommand{{{key}}}{{{value}}}\n")
+   point = sub[(sub["theta_1"] == choice["theta_1"]) & (sub["theta_3"] == choice["theta_3"])]
+   # hollow square marker
+   ax.plot(point["theta_1"], point[col_to_plot], marker = "s", color = "black", markersize = 8, fillstyle = 'none', zorder = 1000)
+   
+   
+   eta_plus = choice["theta_1"]
+   eta_minus = 1 - eta_plus
+   lambda_plus = choice["theta_3"]
+   lambda_minus = 0.5
+   gamma_plus = 0.2
+   gamma_minus = 0.1 
+   
+
+   inset_ax = [inset_1, inset_2, inset_3, inset_4][i]
     
+    
+   expected_modularity = point[col_to_plot].values[0]
+   actual_modularity = -10
+   
+   while not np.isclose(expected_modularity, actual_modularity, atol = 0.001):
+    
+      H = xgi.Hypergraph([[0, 1, 2], [3, 4, 5]])
+      H.set_node_attributes({0 : 0, 1 : 0, 2 : 0, 3 : 1, 4 : 1, 5 : 1}, name = "label")
+      growing_hypergraph = GH(H, [0, 1], eta_plus, eta_minus)
+      growing_hypergraph.add_hyperedge(n_steps, gamma_plus, gamma_minus, lambda_plus, lambda_minus)
+      actual_modularity = clique_projection_modularity(growing_hypergraph)
+      print(f"Expected modularity: {expected_modularity}, actual modularity: {actual_modularity}") 
+    
+   labels = growing_hypergraph.get_labels()
+   H = growing_hypergraph.H
+   
+   
+   H.cleanup(isolates = True)
+   H.set_edge_attributes({e : H.edges.size(e) for e in H.edges}, name = "size")
+   
+   H = growing_hypergraph.H
+   pos = xgi.barycenter_spring_layout(H, seed=1)
+   pos = {i : 0.1*pos[i] for i in H.nodes}
+   
+   node_border_colors = ["black" if labels[i] == 0 else "white" for i in H.nodes]        
+
+   xgi.draw(H, 
+         pos = pos,
+         node_fc=labels, 
+         ax=inset_ax,
+         node_fc_cmap=plt.cm.gray_r, 
+         edge_fc_cmap=plt.cm.viridis,
+         edge_vmin=0, edge_vmax=10,
+         edge_lw = 0,
+         node_ec = node_border_colors,
+         node_lw = .3, 
+         node_size = 5,
+         hull = True, 
+         radius = 0.005, 
+         alpha = 0.2,
+         )
+
+plt.savefig("fig/community-structure-sims-combined.png", dpi=300, bbox_inches="tight", pad_inches=0.0)
